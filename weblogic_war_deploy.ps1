2aaaaa4ea00cf9dd6cd78169943b8a6ae342f15e
Shahnawaz Shaikh <107258940+ShahnawazShaikhh@users.noreply.github.com>
2024-08-02 12:10:10 +0530

    Update weblogic_war_deploy.ps1

--- weblogic_war_deploy.ps1
+++ weblogic_war_deploy.ps1
@@ -9,7 +9,7 @@ $target = "AdminServer"
 
 # Deploy the application using weblogic.Deployer
 #"YOU CAN FIND WEBLOGIC.JAR IN GIVEN PATH:- Oracle_Home\wlserver\server\lib\weblogic.jar (WEBLOGIC.DEPLOYER IS PRESENT IN THE WEBLOGIC.JAR AS A CLASSPATH(CP))"
-java -cp "/home/oracle/Oracle/Middleware/Oracle_Home/wlserver/server/lib/weblogic.jar" weblogic.Deployer `
+java -cp "/home/oracle/Weblogic/Oracle/Middleware/oracle_Home/wlserver/server/lib/weblogic.jar" weblogic.Deployer `
  -verbose `
  -noexit `
  -adminurl $adminUrl `
